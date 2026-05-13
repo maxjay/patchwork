639fd607cc5a697ccc68232fd002dc0fe12f4687
Max <35901228+maxjay@users.noreply.github.com>
2026-05-13 10:59:08 +0000

    added basic apis

--- src/engine.ts
+++ src/engine.ts
@@ -4,4 +4,19 @@ export class Engine<T = any> {
   constructor(base: T) {
     this.base = base;
   }
+
+  add(path: string, value: any): void {
+  }
+
+  replace(path: string, value: any): void {
+  }
+
+  delete(path: string): void {
+  }
+
+  move(from: string, to: string): void {
+  }
+
+  copy(from: string, to: string): void {
+  }
 }
